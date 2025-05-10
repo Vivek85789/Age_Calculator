bbe56e0f4866b0569ca51186dc34e3ee14b5c451
Vivek85789 <kumarvivek8034@gmail.com>
2025-05-10 13:01:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculateAge() {
+    var birthdateInput = document.getElementById('birth').value;
+    var birthdate = new Date(birthdateInput);
+    var today = new Date();
+
+    var age = today.getFullYear() - birthdate.getFullYear();
+    var month = today.getMonth() - birthdate.getMonth();
+    var day = today.getDate() - birthdate.getDate();
+
+    if(today.getMonth() < birthdate.getMonth() || (today.getMonth() === birthdate.getMonth() && today.getDate() < birthdate.getDate())){
+        age--;
+        month = 12 + today.getMonth() - birthdate.getMonth();
+        dat = today.getDate() - birthdate.getDate();
+    }
+document.getElementById('result').innerHTML = "Your age is:" + age + "years, " + month + "months and " + day + "days";
+}
